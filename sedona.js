9ff187e3a1f4eeaf8e71337fc2d119adecb70fa8
Andrew Kross <andkross@yandex.ru>
2019-08-04 20:23:28 +0300

    использован валидатор, автопрефиксер и минимизатор

--- sedona.js
+++ sedona.js
@@ -1,9 +1,7 @@
 var slide = document.querySelector('.form-header');
 var form = document.querySelector('.hotel-search-form');
 
-
 slide.addEventListener ("click", function(evt) {
   evt.preventDefault();
   form.classList.toggle("hotel-search-form-hidden");
-
 });
