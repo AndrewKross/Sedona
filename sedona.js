727ab6a222708c396e9e045154b6d2f8387cc9cf
Andrew Kross <andkross@yandex.ru>
2019-08-04 20:05:25 +0300

    сайт завершен

---
+++ sedona.js
@@ -0,0 +1,9 @@
+var slide = document.querySelector('.form-header');
+var form = document.querySelector('.hotel-search-form');
+
+
+slide.addEventListener ("click", function(evt) {
+  evt.preventDefault();
+  form.classList.toggle("hotel-search-form-hidden");
+
+});
